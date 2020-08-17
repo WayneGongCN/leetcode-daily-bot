8f0ad7d1bf62058176f5e0afe6569c9a72ab330e
wayne <zowiegong@gmail.com>
2020-08-17 09:54:00 +0800

    update api

--- index.js
+++ index.js
@@ -1,64 +1,65 @@
-const { argv } = require('yargs')
-const axios = require('axios')
+const { argv } = require("yargs");
+const axios = require("axios");
 
-
-const getQuestion = ({ cookie }) => {
-  const date = new Date()
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const postOptions = { headers: { cookie, 'Content-Type': 'application/json' } }
+const getQuestion = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const postOptions = {
+    headers: { "Content-Type": "application/json" },
+  };
   const postData = {
-    variables: { year, month },
-    operationName: 'dailyQuestionRecords',
-    query: 'query dailyQuestionRecords($year: Int!, $month: Int!) {\n  dailyQuestionRecords(year: $year, month: $month) {\n    date\n    question {\n      questionId\n      questionTitleSlug\n      translatedTitle\n    }\n  }\n}\n'
-  }
+    variables: {},
+    operationName: "questionOfToday",
+    query:
+      "query questionOfToday {\n  todayRecord {\n    question {\n      questionFrontendId\n      questionTitleSlug\n      __typename\n    }\n    lastSubmission {\n      id\n      __typename\n    }\n    date\n    userStatus\n    __typename\n  }\n}\n",
+  };
 
-  return axios.post('https://leetcode-cn.com/graphql', postData, postOptions)
-    .then(res => {
-      const questions = res.data.data && res.data.data.dailyQuestionRecords
-      const todayData = questions[0] || null
-      if (!todayData) throw new Error(JSON.stringify(res.data))
-      return todayData
-    })
-}
+  return axios
+    .post("https://leetcode-cn.com/graphql", postData, postOptions)
+    .then((res) => {
+      const todayRecord = res.data.data && res.data.data.todayRecord;
+      const todayData = todayRecord[0] || null;
+      if (!todayData) throw new Error(JSON.stringify(res.data));
+      return todayData;
+    });
+};
 
-
-const sendWechatMsg = ({ question, hookUrl, chatid }) => {
-  const { questionId, questionTitleSlug, translatedTitle } = question.question
-  const questionUrl = `https://leetcode-cn.com/problems/${questionTitleSlug}/`
-  const mdContent = `**Leetcode 每日一题 ${question.date}**\n[**#${questionId} ${translatedTitle}**](${questionUrl})`
+const sendWechatMsg = ({ question: todayData, hookUrl, chatid }) => {
+  const { questionFrontendId, questionTitleSlug } = todayData.question;
+  const questionUrl = `https://leetcode-cn.com/problems/${questionTitleSlug}/`;
+  const mdContent = `**Leetcode 每日一题 ${todayData.date} [#${questionFrontendId}](${questionUrl})**\n[**${questionTitleSlug}**](${questionUrl})`;
   const wechatMsg = {
-    chatid: argv.chatid || '',
+    chatid: argv.chatid || "",
     markdown: { content: mdContent },
     msgtype: "markdown",
-  }
+  };
 
-  return axios.post(hookUrl, wechatMsg)
-    .then(res => {
-      if (res.data && res.data.errcode !== 0) throw new Error(JSON.stringify(res.data))
-      return res.data
-    })
-}
+  return axios.post(hookUrl, wechatMsg).then((res) => {
+    if (res.data && res.data.errcode !== 0)
+      throw new Error(JSON.stringify(res.data));
+    return res.data;
+  });
+};
 
+async function main() {
+  const { hookUrl, chatid } = argv;
+  console.log("argv: ", argv);
 
-async function main () {
-  const { cookie, hookUrl, chatid } = argv
-  console.log('argv: ', argv)
-  
-  if (!cookie || !hookUrl || chatid === undefined) {
-    console.error('params error', argv)
-    return process.exit(1)
+  if (!hookUrl || !chatid) {
+    console.error("params error", argv);
+    return process.exit(1);
   } else {
-    const question = await getQuestion({ cookie }).catch(console.error)
-    console.log('question: ', question)
-    if (!question) return process.exit(1)
+    const todayData = await getQuestion().catch(console.error);
+    console.log("todayData: ", todayData);
+    if (!todayData) return process.exit(1);
 
-    await sendWechatMsg({ question, hookUrl, chatid })
+    await sendWechatMsg({ question: todayData, hookUrl, chatid })
       .then(console.log)
-      .catch(e => {
-        console.error(e)
-        process.exit(1)
-      })
+      .catch((e) => {
+        console.error(e);
+        process.exit(1);
+      });
   }
 }
-main()
+main();
